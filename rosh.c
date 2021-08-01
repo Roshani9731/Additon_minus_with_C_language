79069b62d0aeff3cb371352e61ead4199bc0838b
Roshani <roshaninavdiya@gmail.com>
2021-08-01 03:27:33 -0700

    Complited the program of additon

--- rosh.c
+++ rosh.c
@@ -4,5 +4,13 @@ int main()
 {
     int a;
     int b;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+
+    int sum = a + b;
+    printf ("Sum of a and b is: %d",sum);
+
 
 }
