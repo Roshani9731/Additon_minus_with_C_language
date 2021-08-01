a356880eb2f7ac35115205166ef185695997153a
Roshani <roshaninavdiya@gmail.com>
2021-08-01 03:30:59 -0700

    Complited the program of minus

--- rosh.c
+++ rosh.c
@@ -4,5 +4,11 @@ int main()
 {
     int a;
     int b;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
 
+    int minus = a - b;
+    printf("Minus of a and b is %d .",minus);
 }
